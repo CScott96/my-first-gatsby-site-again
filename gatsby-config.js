0b580a89e413eb796704b1e6ccc65c4dfdf2329f
CScott96 <cameron.scott1996@gmail.com>
2021-07-30 20:16:59 +0100

    Finished Gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My First Gatsby Site Again",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud",
-  "gatsby-plugin-image",
-  "gatsby-plugin-sharp",
-],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
